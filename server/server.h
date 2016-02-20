32360d150717c7cea340bdd638eef647fb7f01db
Michael Opara <iamopara@gmail.com>
2016-02-20 13:30:24 -0500

    Create server.h

---
+++ server/server.h
@@ -0,0 +1,24 @@
+#define INPUT_SIZE  6
+
+#define RX 2
+#define TX 3
+
+#define NUNCHUCK_ADDRESS 0x52
+#define MEMORY_ADDRESS   0x40
+
+#define NULL_BYTE 0x00
+
+#define DECRYPT_BYTE 0x17
+
+#define MIN_POS_X 34
+#define MAX_POS_X 233
+
+#define MIN_POS_Y 24
+#define MAX_POS_Y 223
+
+#define MIN_VAL -100
+#define MAX_VAL 100
+
+#define BAUD_RATE 9600
+
+#define DELAY_TIME 10
