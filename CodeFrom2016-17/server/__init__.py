7e976229d099aa5971e8834ffb9cd03d5bb753b3
Aaron Olkin <aaronolkin@gmail.com>
2017-01-17 00:46:07 +0000

    Removed generic settings file, added demo BUTN receiver

--- CodeFrom2016-17/server/__init__.py
+++ CodeFrom2016-17/server/__init__.py
@@ -6,3 +6,8 @@ class Bot:
 
     def remove_client(self, c):
         pass
+
+    def BUTN(self, id, val):
+        fd = open("/sys/class/gpio/gpio{}/value".format(id+14), "w")
+        print(id, val, fd.name)
+        fd.write(str(int(val))+"\n")
