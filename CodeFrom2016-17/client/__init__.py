dd4a2919a190b7fe5f8ae1666ef61ea611eef6b2
Aaron Olkin <aaronolkin@gmail.com>
2017-01-19 18:59:47 -0500

    Finished reliability updates to client video processing

--- CodeFrom2016-17/client/__init__.py
+++ CodeFrom2016-17/client/__init__.py
@@ -6,8 +6,8 @@ from subprocess import Popen, DEVNULL, PIPE, STDOUT
 import atexit
 
 VIDEO_PORT = 6868
-CMD1 = ["netcat", "{address}", "{port}".format(port=VIDEO_PORT)]
-CMD2 = ["mplayer", "-fps", "60", "-cache", "1024", "-cache-min", "1", "-"]
+CMD1 = ("netcat",)
+CMD2 = ("mplayer", "-fps", "60", "-cache", "1024", "-cache-min", "1", "-")
 
 VIDOPTS = [
     {"width": 1920, "height": 1080},
@@ -31,7 +31,7 @@ class Client:
     def video(self, toggle=False):
         if hasattr(self, "proc2"):
             self.proc2.kill()
-            debug(self.proc2.wait(1))
+            self.proc2.wait(1)
         if hasattr(self, "proc1"):
             self.proc1.kill()
             self.proc1.wait(1)
@@ -39,11 +39,12 @@ class Client:
             self.vidopts += 1
             if self.vidopts >= len(VIDOPTS):
                 self.vidopts = 0
-        CMD1[1] = CMD1[1].format(address=self.server)
-        self.proc1 = Popen(CMD1, stdout=PIPE)
+        self.protocol.send_message("VSTR", VIDEO_PORT, 0, VIDOPTS[self.vidopts])
+        
+    def VSTR(self, port, val=None):
+        cmdline = CMD1 + (self.server, str(port))
+        self.proc1 = Popen(cmdline, stdout=PIPE)
         self.proc2 = Popen(CMD2, stdin=self.proc1.stdout, stderr=STDOUT, stdout=get_log_fd("mplayer"))
         self.proc1.stdout.close()
         atexit.register(self.proc1.kill)
         atexit.register(self.proc2.kill)
-        self.protocol.send_message("VSTR", VIDEO_PORT, 0, VIDOPTS[self.vidopts])
-        
