ce1450ff5b5a817c6095ede2509da5ebcb58e02f
Aaron Olkin <aaronolkin@gmail.com>
2017-02-11 02:10:59 -0500

    Wrote delegation and threading code for movement and turret

--- CodeFrom2016-17/server/__init__.py
+++ CodeFrom2016-17/server/__init__.py
@@ -2,13 +2,22 @@
 from botnet.logging import *
 
 from .video import Video
+from .turret import GunController, pan, tilt
+from .move import MovementController
 
 class Bot:
     def __init__(self):
         self.video = Video()
+        self.move = MovementController()
+        self.gun = GunController()
     
     def add_client(self, c, peer):
         self.protocol = c
+        try:
+            self.move.start()
+            self.gun.start()
+        except RuntimeError:
+            pass
 
     def remove_client(self, c):
         self.video.kill()
@@ -22,9 +31,23 @@ class Bot:
         self.protocol.send_message("VSTR", port)
 
     def QUIT(self, btn, val, msg):
+        self.move.stop.set()
+        self.gun.stop.set()
         log(msg)
         log("Received QUIT from client. Quitting...")
         exit()
         
     def BUTN(self, id, val):
-        pass
+        if id == 1:
+            self.gun.firing = val
+        elif id == 6:
+            self.move.right(val)
+        elif id == 7:
+            self.move.left(val)
+
+    def AXIS(self, id, val):
+        if id == 0: self.move.forward(val)
+        elif id == 1: self.move.strafe(val)
+        elif id == 2: pan(val)
+        elif id == 3: tilt(val)
+            
---
+++ CodeFrom2016-17/server/turret.py
@@ -0,0 +1,38 @@
+
+from threading import Thread, Lock, Event
+
+import time
+
+class GunController(Thread):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.firing = False
+
+    ### THIS IS THE FUNCTION
+    def fire():
+        """This function should fire one round from the turret."""
+        print("Shooting Turret")
+        return time.sleep(.2)
+        raise NotImplemented()
+
+    def run(self):
+        self.stop = Event()
+        while not self.stop.is_set():
+            if self.firing:
+                try:
+                    self.fire()
+                except:
+                    time.sleep(1) # Don't retry instantly on failure
+
+### Ignore below for now
+
+def pan(speed):
+    """This function should turn on the pan motor at the specified speed,
+       positive values are to the right, negative to the left. Zero is off.
+       The input will range from -1 to +1. `pan` should return instantly."""
+    raise NotImplemented()
+
+def tilt(speed):
+    """This function should behave like the `pan` function, but for the tilt
+       tilt motor. It should return instantly."""
+    raise NotImplemented()
