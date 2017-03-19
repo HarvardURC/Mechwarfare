6c0d22e507b2bb28e62520d530857fd519650773
Aaron Olkin <aaronolkin@gmail.com>
2017-03-18 23:16:57 -0400

    Don't go to home when leaving STOP

--- CodeFrom2016-17/server/move.py
+++ CodeFrom2016-17/server/move.py
@@ -111,7 +111,8 @@ class MovementController(Thread):
         self._last_direction = None
         while not self.stop.is_set():
             if self._last_direction != self._direction.name:
-                goToHomeFromAnyPosition()
+                if self._last_direction != Direction.STOP:
+                    goToHomeFromAnyPosition()
                 self._last_direction = self._direction.name
             if self._speed != 0:
                 getattr(self.motors, self._direction.name)(self._speed)
