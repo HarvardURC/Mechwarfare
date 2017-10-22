93870455970adb9e19fee80872cd0fbe78c07f9f
Garrett Tanzer <gtanzer@college.harvard.edu>
2017-10-22 16:18:23 -0400

    Python to V-REP interface

--- 2017-18/V-REP/README.md
+++ 2017-18/V-REP/README.md
@@ -4,6 +4,12 @@ Useful links:
 
 [V-REP download (pro^edu)](http://www.coppeliarobotics.com/downloads.html)
 
+[How to build a robot model](http://www.coppeliarobotics.com/helpFiles/en/buildingAModelTutorial.htm)
+
+[How to build a robot model 2](http://www.coppeliarobotics.com/helpFiles/en/hexapodTutorial.htm)
+
+---
+
 [Server Side](http://www.coppeliarobotics.com/helpFiles/en/remoteApiServerSide.htm)
 
 [Client Side](http://www.coppeliarobotics.com/helpFiles/en/remoteApiClientSide.htm)
@@ -12,6 +18,10 @@ Useful links:
 
 [Video Tutorial](https://www.youtube.com/watch?v=SQont-mTnfM)
 
+---
+
 You need `remoteApi.dll`, `remoteApi.dylib`, or `remoteApi.so` from `[V-REP install] > programming > remoteApi > lib > lib` in your working directory in order to interface with Python.
 
 We access the V-REP scene `test.ttt` with as a remote API client in `test.py`.
+
+
--- 2017-18/V-REP/test.py
+++ 2017-18/V-REP/test.py
@@ -1,7 +1,33 @@
-# To begin looking for clients, call
-# simExtRemoteApiStart(19999)
-# from some threaded script in V-REP.
+"""
+To begin looking for clients, call
+simExtRemoteApiStart(19999)
+from some threaded script in V-REP.
 
-import vrep
+Python client remote API documentation:
+http://www.coppeliarobotics.com/helpFiles/en/remoteApiFunctionsPython.htm
+"""
 
+import vrep, sys
 
+vrep.simxFinish(-1)     # close any existing connections
+cID = vrep.simxStart('127.0.0.1', 19999, True, True, 5000, 5)
+
+if cID != -1:
+    print("Connected to remote API server")
+else:
+    print("Connection failed")
+    sys.exit(1)
+
+err, lm = vrep.simxGetObjectHandle(cID, 'Pioneer_p3dx_leftMotor', vrep.simx_opmode_oneshot_wait)
+if err != 0:
+    print("'Pioneer_p3dx_leftMotor' couldn't be found")
+    sys.exit(1)
+
+err, rm = vrep.simxGetObjectHandle(cID, 'Pioneer_p3dx_rightMotor', vrep.simx_opmode_oneshot_wait)
+if err != 0:
+    print("'Pioneer_p3dx_rightMotor' couldn't be found")
+    sys.exit(1)
+
+while 1:
+    vrep.simxSetJointTargetVelocity(cID, lm, 0.5, vrep.simx_opmode_oneshot)
+    vrep.simxSetJointTargetVelocity(cID, rm, 0.5, vrep.simx_opmode_oneshot)
