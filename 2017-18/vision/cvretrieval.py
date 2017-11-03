85a0c8cdc3cb8f9642c1dfd7c522df4d7030424d
Leor <leor.fishman@gmail.com>
2017-11-02 21:10:22 -0400

    Added optimizations

--- 2017-18/vision/cvretrieval.py
+++ 2017-18/vision/cvretrieval.py
@@ -8,29 +8,48 @@ files=os.listdir(root)
 found=False
 wrong=0.
 right=0.
-for file in files:
-    found=False
-    potentialcoords= []
-    numberclauses=re.findall(r"\d+x\d+", file)
-    for numberclause in numberclauses:
-        beginning, end=numberclause.split('x')
-        beginning=int(beginning)
-        end=int(end)
-        potentialcoords.append((beginning, end))
-    image = root + "\\" + file
-    image = image.replace("\\", "\\\\")
-    squares, x, y=sq.findsquares(image)
-    for coord in potentialcoords:
-        if abs(x-coord[0])>10. or abs(y-coord[1])>10.:
-            pass
-        else:
-            found=True
-            print(coord)
-            break
-    if not found:
-        wrong+=1.
-    else:
-        right+=1.
-print(right/(wrong+right))
+maxthrs1=0
+maxthrs2=0
+maxperform=0
+for k in range(2):
+    for i in range(0, 255, 5):
+        for j in range(i+1, 255, 5):
+            wrong = 0.
+            right = 0.
+            for file in files:
 
-print(str(potentialcoords))
+                found=False
+                potentialcoords= []
+                numberclauses=re.findall(r"\d+x\d+", file)
+                if 'NaN' in file:
+                    potentialcoords=[(-1, -1)]
+                for numberclause in numberclauses:
+                    beginning, end=numberclause.split('x')
+                    beginning=int(beginning)
+                    end=int(end)
+                    potentialcoords.append((beginning, end))
+                image = root + "\\" + file
+                image = image.replace("\\", "\\\\")
+                squares, x, y=sq.findsquares(image, i, j, k)
+                for coord in potentialcoords:
+                    #print(str(coord))
+                    if abs(x-coord[0])>15. or abs(y-coord[1])>15.:
+                        pass
+                    else:
+                        found=True
+                        #print(coord)
+                        break
+                if not found:
+                    wrong+=1.
+                    #print(file + " incorrect")
+                else:
+                    right+=1.
+                    #print(file + " correct")
+            perform=right/(wrong+right)
+            if perform>maxperform:
+                maxperform=perform
+                maxthrs1=i
+                maxthrs2=j
+            print(right/(wrong+right))
+print((maxperform, maxthrs1, maxthrs2))
+#print(str(potentialcoords))
