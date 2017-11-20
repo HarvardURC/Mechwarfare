f289f03872e5901c8e1e01015ea81365eda410f9
Chris <chris@CalMBP.local>
2017-11-19 19:41:49 -0500

    ik changes

--- 2017-18/server/controller.py
+++ 2017-18/server/controller.py
@@ -19,7 +19,7 @@ def fix_angles(angles):
 
 # given a list of angles and a time, maintains angle positions for time seconds
 def maintain_pos(time, angles):
-    for i in range(40 * time):
+    for i in range(40 * int(time)):
         client.send(hlsockets.SERVO, angles)
         sleep(.025)
 
@@ -29,32 +29,49 @@ def reset(time=10):
     maintain_pos(time, angles)
 
 # given body, list of claw positions, pitch, and roll, stands at pitch and roll for time seconds
-def stand(body, claws, pitch=0, roll=0, time=10):
+def stand(pitch=0, roll=0, time=10):
     angles = fix_angles(ik.extract_angles(body, claws, pitch, roll))
     maintain_pos(time, angles)
+
+# stands on tippy toes
+def tippytoes():
+    angles = fix_angles([0, -90, 0, 0, -90, 0, 0, -90, 0, 0, -90, 0])
+    for i in range(20):
+        client.send(hlsockets.SERVO, angles)
+        sleep(.025)
+    reset(1)
+
+# jumps a little bit
+def jump():
+    angles = fix_angles([0, -90, -90, 0, -90, -90, 0, -90, -90, 0, -90, -90])
+    for i in range(20):
+        client.send(hlsockets.SERVO, angles)
+        sleep(.025)
+    reset(1)
 
 # does the robot boogie for time seconds
 def wiggle(time=10):
     for j in range(time):
         for i in range(10):
-            client.send(hlsockets.SERVO, fix_angles(ik.extract_angles(body, claws, 15, 0)))
+            client.send(hlsockets.SERVO, fix_angles(ik.extract_angles(body, claws, 20, 0)))
             sleep(.025)
         for i in range(10):
-            client.send(hlsockets.SERVO, fix_angles(ik.extract_angles(body, claws, -10, 15)))
+            client.send(hlsockets.SERVO, fix_angles(ik.extract_angles(body, claws, -10, 20)))
             sleep(.025)
         for i in range(10):
-            client.send(hlsockets.SERVO, fix_angles(ik.extract_angles(body, claws, 15, 0)))
+            client.send(hlsockets.SERVO, fix_angles(ik.extract_angles(body, claws, 20, 0)))
             sleep(.025)
         for i in range(10):
-            client.send(hlsockets.SERVO, fix_angles(ik.extract_angles(body, claws, -10, -15)))
+            client.send(hlsockets.SERVO, fix_angles(ik.extract_angles(body, claws, -10, -20)))
             sleep(.025)
         
 
 client = hlsockets.UDSClient()
 client.open(hlsockets.CONTROLLER)
+reset(2)
 while True:
-    reset(2)
-    wiggle()
+    tippytoes()
+    jump()
 client.close()
 
 
