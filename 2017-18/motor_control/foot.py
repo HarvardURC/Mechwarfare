f04705c6e0e39b5e15db26a2593571868a38cc52
Garrett Tanzer <gtanzer@college.harvard.edu>
2018-01-28 19:55:53 -0500

    spoopy

--- 2017-18/motor_control/foot.py
+++ 2017-18/motor_control/foot.py
@@ -2,51 +2,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from mpl_toolkits.mplot3d import Axes3D
-from math import pow
+from math import pow, pi
 
 t = 0
 step = 0.01
 
-homes = [[3, -3, 1], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
+homes = [[3, -3, 1], [3, 3, 1], [-3, 3, 1], [-3, -3, 1]]
 ends = [list(home) for home in homes]
 coords = [[home[0], home[1], 0] for home in homes]
-vs = [[5, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
-ωs = [1, 0, 0, 0]
+vs = [5, 0, 0]
+ω = 1
+lift_height = 1
 
 points = [[[],[],[]], [[],[],[]], [[],[],[]], [[],[],[]]]
 
-beats = 4
+beats = 12
 beatlen = 1
 
-while t < (beats * beatlen):
-    if 2.0 < (t / beatlen) % 4 < 2.02:
-        vs[0][0] = 0
-        vs[0][1] = 2
-    if (t / beatlen) % 4 < 3:
-        points[0][0].append(coords[0][0])
-        points[0][1].append(coords[0][1])
-        points[0][2].append(coords[0][2])
-        coords[0][0] += (-vs[0][0] + coords[0][1]*ωs[0]) * step
-        coords[0][1] += (-vs[0][1] - coords[0][0]*ωs[0]) * step
+def update_leg(i):
+    tphase = t + i*beatlen
+    
+    # on ground
+    if (tphase / beatlen) % 4 < 3:
+        points[i][0].append(coords[i][0])
+        points[i][1].append(coords[i][1])
+        points[i][2].append(coords[i][2])
+        coords[i][0] += (-vs[0] + coords[i][1]*ω) * step
+        coords[i][1] += (-vs[1] - coords[i][0]*ω) * step
+    
+    # in air
     else:
-        if ends[0][0] == homes[0][0]:
-            ends[0][0] = coords[0][0]
-            ends[0][1] = coords[0][1]
-            ends[0][2] = coords[0][2]
-        points[0][0].append(coords[0][0])
-        points[0][1].append(coords[0][1])
-        points[0][2].append(coords[0][2])
-        coords[0][0] += (homes[0][0] - ends[0][0]) * (step / beatlen)
-        coords[0][1] += (homes[0][1] - ends[0][1]) * (step / beatlen)
-        if (t % beatlen)/beatlen < 1/4:
-            coords[0][2] += (homes[0][2] - ends[0][2]) * (step / (beatlen / 4))
-        if (t % beatlen)/beatlen > 3/4:
-            coords[0][2] -= (homes[0][2] - ends[0][2]) * (step / (beatlen / 4))
+        if(-0.01 < tphase % beatlen < 0.01):
+            ends[i][0] = coords[i][0]
+            ends[i][1] = coords[i][1]
+            ends[i][2] = coords[i][2]
+        points[i][0].append(coords[i][0])
+        points[i][1].append(coords[i][1])
+        points[i][2].append(coords[i][2])
+        coords[i][0] += (homes[i][0] - ends[i][0]) * step / beatlen
+        coords[i][1] += (homes[i][1] - ends[i][1]) * step / beatlen
+        
+        # lift foot
+        if (tphase % beatlen)/beatlen < 1/4:
+            coords[i][2] += (homes[i][2]) * (step / (beatlen / 4))
+            
+        # drop foot
+        if (tphase % beatlen)/beatlen > 3/4:
+            coords[i][2] -= (homes[i][2]) * (step / (beatlen / 4))
+
+while t < (beats * beatlen):
+    update_leg(0)
+    if(t > 3*beatlen):
+        update_leg(1)
+    if(t > 2*beatlen):
+        update_leg(2)
+    if(t > 1*beatlen):
+        update_leg(3)
     t += step
 
-colors = cm.rainbow(np.linspace(0, 1, len(points[0][0])))
+for i in range(len(points)):
+    colors = cm.rainbow(np.linspace(0, 1, len(points[i][0])))
+    fig = plt.figure()
+    a = fig.add_subplot(111, projection='3d')
+    a.scatter(xs=points[i][0], ys=points[i][1], zs=points[i][2], c=colors)
 
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(xs=points[0][0], ys=points[0][1], zs=points[0][2], c=colors)
+plt.interactive(False)
 plt.show()
