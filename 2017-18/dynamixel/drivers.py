2dd3887a3ce1a7f7df9f90cd1a4a9290704e7155
Garrett Tanzer <gtanzer@college.harvard.edu>
2018-03-04 19:47:45 -0500

    dynamixel server probably

--- 2017-18/dynamixel/drivers.py
+++ 2017-18/dynamixel/drivers.py
@@ -1,6 +1,6 @@
 import os
 
-# random set-up stuff
+# random setup stuff
 if os.name == 'nt':
     import msvcrt
     def getch():
@@ -31,8 +31,7 @@ ADDR_MX_PRESENT_POSITION    = 36
 
 TORQUE_ENABLE               = 1                             # Value for enabling the torque
 TORQUE_DISABLE              = 0                             # Value for disabling the torque
-DXL_MINIMUM_POSITION_VALUE  = 100                           # Dynamixel will rotate between this value
-DXL_MAXIMUM_POSITION_VALUE  = 4000                          # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+
 DXL_MOVING_STATUS_THRESHOLD = 10                            # Dynamixel moving status threshold
 
 ESC_ASCII_VALUE             = 0x1b
@@ -41,16 +40,17 @@ COMM_SUCCESS                = 0                             # Communication Succ
 COMM_TX_FAIL                = -1001                         # Communication Tx Failed
 
 # will be updated at runtime
-IDS = []
-PORT_NUM = 0
+IDS = None
+PORT_NUM = None
 
 
-# takes a list of id numbers as a parameter
+# takes a list of id numbers as a parameter and default offsets (same dimension)
 # returns -1 on failure
-def init_motors(ids_list):
+def init_motors(ids_list, offsets):
     global PORT_NUM, IDS
 
     IDS = ids_list
+    
     PORT_NUM = dynamixel.portHandler(DEVICENAME)
 
     # initialize PacketHandler structs
@@ -101,15 +101,16 @@ def deinit_motors():
 
 # takes a list of target positions
 # - pos_list must have the same dimension as ids_list from init_motors()
-# returns -1 on failure
+# returns -1 on failure and prints error
 def set_target_positions(pos_list):
 
     if(len(pos_list) != len(IDS)):
-        print("pos_list doesn't have the same dimension as ids_list you fucker")
+        print("pos_list doesn't have the same dimension as ids_list")
         return -1
 
     for i in range(len(pos_list)):
         # write goal position
+        
         dynamixel.write2ByteTxRx(PORT_NUM, PROTOCOL_VERSION, IDS[i], ADDR_MX_GOAL_POSITION, pos_list[i])
         dxl_comm_result = dynamixel.getLastTxRxResult(PORT_NUM, PROTOCOL_VERSION)
         dxl_error = dynamixel.getLastRxPacketError(PORT_NUM, PROTOCOL_VERSION)
@@ -123,7 +124,7 @@ def set_target_positions(pos_list):
 
 from time import sleep
 
-err = init_motors([2,3])
+err = init_motors([2,3], [0, 90])    # should be 12 in final version
 err = set_target_positions([300, 300])
 sleep(1)
 err = set_target_positions([400, 400])
