b41a69b77f4f195cbdf9340a16c2ebb7e94558e2
cal-miller-harvard <calder_miller@college.harvard.edu>
2018-03-30 00:11:45 -0400

    Server and timing and stuff

--- 2017-18/frontend/webtest.py
+++ 2017-18/frontend/webtest.py
@@ -1,5 +1,20 @@
 from flask import Flask, request, render_template
 import json
+from threading import Thread
+from apscheduler.schedulers.background import BackgroundScheduler
+
+
+dt = 0.05
+state = "hi"
+
+def update_robot():
+	global state
+	print(state)
+
+def start_server():
+	global app
+	app.run()
+   
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,8 +23,15 @@ def hello():
 
 @app.route('/handler', methods=['POST'])
 def slidey():
-	x = json.loads(request.data.decode('utf-8'))
-	print(x) #THIS IS THE OUTPUT DICTIONARY
+	global state
+	state = json.loads(request.data.decode('utf-8'))
 	return ""
 
-app.run()
+sched = BackgroundScheduler()
+sched.add_job(update_robot, 'interval', seconds=dt)
+sched.start()
+
+start_server()
+
+#serverthread = Thread(target=start_server, daemon=True)
+#serverthread.start()
