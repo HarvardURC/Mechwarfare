9f4e6ce8e0fcce56d17ebd99d6601a50d4ec2740
Garrett Tanzer <gtanzer@college.harvard.edu>
2018-03-31 20:16:44 -0400

    test

---
+++ 2017-18/startup/startup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# move this file to /etc/init.d
+# run `chmod 755 startup.sh` to make the script executable
+# include sudo password in a text file "password.txt"
+
+git pull
+sudo -S chmod 666 /dev/ttyUSB0 < password.txt
+
+# add more of these (backgrounded with &) if you're using multiple processes
+
+python ~/Mechwarfare/2017-18/server/server.py
