b086667a3ad4b2c1f2c3d082bea520227ec31dcf
Leor <leor.fishman@gmail.com>
2018-04-22 16:52:12 -0400

    new state changes

--- 2017-18/Fire control/arduinogimbal/arduinogimbal.ino
+++ 2017-18/Fire control/arduinogimbal/arduinogimbal.ino
@@ -12,49 +12,58 @@
 //Write the control loop for the camera to gimbal in a more standard form (regular PID?). Add optional low pass filter to sensor data  DONE
 //Timer interrupts for running state machine at constant frequency; decouple control code  DONE
 
-//NOTE:  STATES NEED TO BE UPDATED FOR NEW PWM CODE
+/*NOTES ON SWITCH INTERPRETATION:  IDLESWITCH ON:  GO IDLE.  GUNSWITCH ON:  ACTIVE FIRE/RELOAD.  JAMSWITCH ON:  UNJAM.*/
 
-//communication defines for comms with Pi and remote control
-#include <SBUS.h>
-SBUS x8r(Serial1);
+
+/*
+  GENERAL STATE DIAGRAM:
+
+  IDLE STATE:  REMAIN IN UNTIL THE IDLE SWITCH IS FLIPPED, AT WHICH POINT TRANSITION TO LOAD STATE.  OTHERWISE DO NOTHING.
+  ALL BELOW STATES TRANSITION TO IDLE IF IDLE SWITCH IS FLIPPED:
+  QUIET LOAD STATE:  IF THE HOPPER HASN'T CAUGHT UP TO THE GUN (LOADED NEW ROUNDS) RUN THE HOPPER, OTHERWISE WAIT FOR GUN COMMAND TO TRANSITION TO ACTIVE FIRE
+  ACTIVE FIRE STATE: RUN GUNMOTOR AND HOPPER MOTOE, TRANSITIONING TO QUIET LOAD IF FIRE BUTTON RELEASED, TRANSITION TO UNJAM IF JAM COMMAND SENT OTHERWISE RUN GUNMOTOR AND HOPPER UNTIL TIME COMES FOR ACTIVE RELOAD
+  ACTIVE RELOAD: SHUT OFF GUN MOTOR TO ALLOW CATCHUP TIME FOR HOPPER, TRANSITION TO QUIET LOAD IF FIRE BUTTON RELEASED, TRANSITION TO UNJAM IF JAM COMMAND SENT, OTHERWISE KEEP GUNMOTOR OFF AND RUN HOPPER UNTIL TIME COMES FOR ACTIVE FIRE
+  UNJAM:  SHUT OFF GUN MOTOR, RUN HOPPER MOTOR BACKWARDS UNTIL TIME FOR UNJAM RUNS OUT, THEN GO TO QUIET LOAD
+*/
+
+
 #define computer Serial
-#define gimbal x8r
-#define SERIAL1_PIN 0
 
-// channel, fail safe, and lost frames data
-float channels[16];
-int state[11];
-uint8_t failSafe;
-uint16_t lostFrames = 0;
-uint16_t gimbals[3];
+
+// Defines for retrieving remote control signals
+
+volatile uint16_t state[9];
+
+volatile unsigned long timers[9];
+
+#define CH1 23
+#define CH2 22
+#define CH3 21
+#define CH4 19
+#define CH5 18
+#define CH6 17
+#define CH7 16
+#define CH8 15
 
 //remote control channels
-#define IDLE_SWITCH 0
-#define GUN_CHANNEL 0
-#define GUN_COMMAND 0
-#define LEG_CHANNEL_1 0
-#define LEG_CHANNEL_2 0
-#define LEG_CHANNEL_3 0
-#define LEG_CHANNEL_4 0
-#define LEG_CHANNEL_5 0
-#define LEG_CHANNEL_6 0
-#define LEG_CHANNEL_7 0
-#define LEG_CHANNEL_8 0
-#define HOPPER_MOTOR 255
-#define GUN_DIR 255
-#define JAM_CHANNEL 0
-#define JAM_COMMAND 255
+#define IDLE_SWITCH 8
+#define GUN_CHANNEL 5
+#define SWITCH_BOUND 1200
+#define JAM_CHANNEL 1
+
 
 //Comms defines for comms with gun subsystems
 int gunmotor = 6;
 int laserpointer = 2;
 int hoppermotor = 9;
 int hopperdir = 8;
-int lightsensor = 14;
+
+#define HOPPER_MOTOR 255
+#define GUN_DIR 255
 
 #define IDLE_THRESHOLD 1000
 #define UPDATE_THRESHOLD 10
-unsigned long idleTimer; // number of milliseconds without recieving data from the controller
+unsigned long idleTimer; //  hold milliseconds since last state update
 void resetIdleTimer()
 {
   idleTimer = millis();
@@ -62,31 +71,100 @@ void resetIdleTimer()
 
 void setup() {
   // begin the SBUS communication
-  x8r.begin();
+
   Serial.begin(9600);
   pinMode(gunmotor, OUTPUT);
   pinMode(laserpointer, OUTPUT);
   pinMode(hoppermotor, OUTPUT);
   pinMode(hopperdir, OUTPUT);
   pinMode(lightsensor, INPUT);
-  pinMode(GUN_CHANNEL, INPUT);
-  pinMode(IDLE_SWITCH, INPUT);
-  pinMode(JAM_CHANNEL, INPUT);
-  pinMode(LEG_CHANNEL_1, INPUT);
-  pinMode(LEG_CHANNEL_2, INPUT);
-  pinMode(LEG_CHANNEL_3, INPUT);
-  pinMode(LEG_CHANNEL_4, INPUT);
-  pinMode(LEG_CHANNEL_5, INPUT);
-  pinMode(LEG_CHANNEL_6, INPUT);
-  pinMode(LEG_CHANNEL_7, INPUT);
-  pinMode(LEG_CHANNEL_8, INPUT);
+   pinMode(CH1,INPUT);
+  pinMode(CH2,INPUT);
+  pinMode(CH3,INPUT);
+  pinMode(CH4,INPUT);
+  pinMode(CH5,INPUT);
+  pinMode(CH6,INPUT);
+  pinMode(CH7,INPUT);
+  pinMode(CH8,INPUT);
+  attachInterrupt(CH1,interrupt1,CHANGE);
+  attachInterrupt(CH2,interrupt2,CHANGE);
+  attachInterrupt(CH3,interrupt3,CHANGE);
+  attachInterrupt(CH4,interrupt4,CHANGE);
+  attachInterrupt(CH5,interrupt5,CHANGE);
+  attachInterrupt(CH6,interrupt6,CHANGE);
+  attachInterrupt(CH7,interrupt7,CHANGE);
+  attachInterrupt(CH8,interrupt8,CHANGE);
 
   idleTimer = millis();
 }
-
+//Reads Pulse length manually -- basically softPWM
+void interrupt1(){
+    if(digitalReadFast(CH1) == 1){         
+      timers[1] = micros();
+    }
+    else {
+      state[1] = micros() - timers[1];
+   }
+}
+void interrupt2(){
+    if(digitalReadFast(CH2) == 1){         
+      timers[2] = micros();
+    }
+    else {
+      state[2] = micros() - timers[2];
+   }
+}
+void interrupt3(){
+    if(digitalReadFast(CH3) == 1){         
+      timers[3] = micros();
+    }
+    else {
+      state[3] = micros() - timers[3];
+   }
+}
+void interrupt4(){
+    if(digitalReadFast(CH4) == 1){         
+      timers[4] = micros();
+    }
+    else {
+      state[4] = micros() - timers[4];
+   }
+}
+void interrupt5(){
+    if(digitalReadFast(CH5) == 1){         
+      timers[5] = micros();
+    }
+    else {
+      state[5] = micros() - timers[5];
+   }
+}
+void interrupt6(){
+    if(digitalReadFast(CH6) == 1){         
+      timers[6] = micros();
+    }
+    else {
+      state[6] = micros() - timers[6];
+   }
+}
+void interrupt7(){
+    if(digitalReadFast(CH7) == 1){         
+      timers[7] = micros();
+    }
+    else {
+      state[7] = micros() - timers[7];
+   }
+}
+void interrupt8(){
+    if(digitalReadFast(CH8) == 1){         
+      timers[8] = micros();
+    }
+    else {
+      state[8] = micros() - timers[8];
+   }
+}
 int stateGun = 0;
-int stateLeg = 0;
-//return condition for fire-idle
+
+//Hopper controller
 void hopperDriver(int inst, int power) {
   switch (inst) {
     case 0:
@@ -106,9 +184,6 @@ void hopperDriver(int inst, int power) {
       break;
   }
 }
-void unjamcode() {
-  hopperDriver(2, HOPPER_MOTOR);
-}
 
 
 // needs code
@@ -119,19 +194,9 @@ int numFire = 150;
 int numReload = 200;
 int numUnjam = 200;
 int stateHold = 0;
-/*While fire button is help, alternate between running and not running gun.  When button is released, run hopper to ensure gun is loaded */
 
 
-/*
-  GENERAL STATE DIAGRAM:
 
-  IDLE STATE:  REMAIN IN UNTIL THE IDLE SWITCH IS FLIPPED, AT WHICH POINT TRANSITION TO LOAD STATE.  OTHERWISE DO NOTHING.
-  ALL BELOW STATES TRANSITION TO IDLE IF IDLE SWITCH IS FLIPPED:
-  QUIET LOAD STATE:  IF THE HOPPER HASN'T CAUGHT UP TO THE GUN (LOADED NEW ROUNDS) RUN THE HOPPER, OTHERWISE WAIT FOR GUN COMMAND TO TRANSITION TO ACTIVE FIRE
-  ACTIVE FIRE STATE: RUN GUNMOTOR AND HOPPER MOTOE, TRANSITIONING TO QUIET LOAD IF FIRE BUTTON RELEASED, TRANSITION TO UNJAM IF JAM COMMAND SENT OTHERWISE RUN GUNMOTOR AND HOPPER UNTIL TIME COMES FOR ACTIVE RELOAD
-  ACTIVE RELOAD: SHUT OFF GUN MOTOR TO ALLOW CATCHUP TIME FOR HOPPER, TRANSITION TO QUIET LOAD IF FIRE BUTTON RELEASED, TRANSITION TO UNJAM IF JAM COMMAND SENT, OTHERWISE KEEP GUNMOTOR OFF AND RUN HOPPER UNTIL TIME COMES FOR ACTIVE FIRE
-  UNJAM:  SHUT OFF GUN MOTOR, RUN HOPPER MOTOR BACKWARDS UNTIL TIME FOR UNJAM RUNS OUT, THEN GO TO QUIET LOAD
-*/
 int gunState(int currState)
 {
   switch (currState) {
@@ -139,7 +204,7 @@ int gunState(int currState)
     case 0:
       hopperDriver(0, 0);
       analogWrite(gunmotor, 0);
-      if (state[IDLE_SWITCH] != 0) {
+      if (state[IDLE_SWITCH] > SWITCH_BOUND) {
         //if no longer idle
         stateHold = 0;
         return 1;
@@ -147,7 +212,7 @@ int gunState(int currState)
       return 0;
     case 1:
       //load state
-      if (state[IDLE_SWITCH] == 0) {
+      if (state[IDLE_SWITCH] < SWITCH_BOUND) {
         //if idle
         stateHold = 0;
         return 0;
@@ -164,7 +229,7 @@ int gunState(int currState)
 
       stateHold++;
       //change number later
-      if (state[GUN_CHANNEL] == GUN_COMMAND) {
+      if (state[GUN_CHANNEL] > SWITCH_BOUND) {
         //move to fire
         stateHold = 0;
         return 2;
@@ -175,17 +240,17 @@ int gunState(int currState)
       analogWrite(gunmotor, GUN_DIR);
       hopperDriver(1, HOPPER_MOTOR);
       stateHold++;
-      if (state[IDLE_SWITCH] == 0) {
+      if (state[IDLE_SWITCH] < SWITCH_BOUND) {
         //if idle
         stateHold = 0;
         return 0;
       }
-      if (state[JAM_CHANNEL] == JAM_COMMAND) {
+      if (state[JAM_CHANNEL] > SWITCH_BOUND) {
         //then unjam
         stateHold = 0;
         return 4;
       }
-      if (state[GUN_CHANNEL] != GUN_COMMAND) {
+      if (state[GUN_CHANNEL] < SWITCH_BOUND) {
         //then enter quiet load state
         stateHold = 0;
         return 1;
@@ -207,12 +272,12 @@ int gunState(int currState)
         stateHold = 0;
         return 0;
       }
-      if (state[JAM_CHANNEL] == JAM_COMMAND) {
+      if (state[JAM_CHANNEL] > SWITCH_BOUND) {
         //then unjam
         stateHold = 0;
         return 4;
       }
-      if (state[GUN_CHANNEL] != GUN_COMMAND) {
+      if (state[GUN_CHANNEL] < SWITCH_BOUND) {
         //then enter quiet load state
         stateHold = 0;
         return 1;
@@ -227,7 +292,7 @@ int gunState(int currState)
       analogWrite(gunmotor, 0);
       hopperDriver(2, HOPPER_MOTOR);
       stateHold++;
-      if (state[IDLE_SWITCH] == 0) {
+      if (state[IDLE_SWITCH] > SWITCH_BOUND) {
         //if idle
         stateHold = 0;
         return 0;
@@ -260,10 +325,10 @@ void legCode()
 
 
 void loop() {
-  //timed interrupt on idle state, leg state sent continuously
+  //timed interrupt on gun and leg state
   if (millis() - idleTimer > UPDATE_THRESHOLD) {
+    
     resetIdleTimer();
-    // detect whenever we send a command to fire the gun
     stateGun = gunState(stateGun);
     legCode();
 
