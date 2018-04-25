3bf478770a5d06aba84908be682385da00933185
cal-miller-harvard <calder_miller@college.harvard.edu>
2018-04-25 00:42:15 -0400

    server-free startup?

--- 2017-18/frontend/webtest.py
+++ 2017-18/frontend/webtest.py
@@ -8,12 +8,37 @@ import macros
 from time import time
 import helpers
 
-
-dt = macros.TIMESTEP
-state = "hi"
+state = {
+    "enable":True,
+    "gohome":False,
+    "vx":0.,
+    "vy":0.,
+    "vz":0.,
+    "omega":0.,
+    "height":macros.DEFAULT_HEIGHT,
+    "pitch":macros.DEFAULT_PITCH,
+    "roll":macros.DEFAULT_ROLL,
+    "yaw":macros.DEFAULT_YAW,
+    "homewistate["timestep"]h":,
+    "homelength":,
+    "timestep":macros.TIMESTEP,
+    "stridelength":macros.STRIDELENGTH,
+    "raisefrac":macros.RAISEFRAC,
+    "raiseh":macros.RAISEH,
+    "liftphase":macros.LIFT_PHASE,
+    "phasefl":macros.phases[0],
+    "phasebl":macros.phases[1],
+    "phasebr":macros.phases[2],
+    "phasefr":macros.phases[3],
+    "useradio": True
+}
 body = init_robot()
 
-ser = serial.Serial('/dev/ttyACM0', 38400, timeout=1)
+try:
+    ser = serial.Serial('/dev/ttyACM0', 38400, timeout=1)
+except:
+    ser = serial.Serial('/dev/ttyACM1', 38400, timeout=1)
+
 offset = (990+2014)/2
 controller_range = (2014-990)/2
 
@@ -23,13 +48,13 @@ def scale(num, max):
 
 def fucking_loop():
     global state
-    if (state != "hi"):
+    if state["enable"]:
         #tv_fl = time()
-        update_robot(body, state, dt)
+        update_robot(body, state, state["timestep"])
         #print("aaaaaa: ",time()-tv_fl)
-        print("vx: ", state["vx"])
-        print("vy: ", state["vy"])
-        print("omega: ", state["omega"])
+        #print("vx: ", state["vx"])
+        #print("vy: ", state["vy"])
+        #print("omega: ", state["omega"])
         #print("pitch: ", state["pitch"])
         #print("roll: ", state["roll"])
         #print("yaw: ", state["yaw"]) 
@@ -37,21 +62,21 @@ def fucking_loop():
 
 def fucking_teensy_loop():
     global state
-    if (state != "hi"):
-        if (bool(state["useradio"])):
-        	if (ser.in_waiting > 0):
-	            msg = ser.readline().decode('utf-8')
-	            msg = [int(i) for i in msg.split(', ')]
-	            #print(msg)
-	            state["vx"] = scale(msg[3], macros.V_MAX)          # forward/backward trans
-	            state["omega"] = scale(msg[4], macros.OMEGA_MAX)   # stationary rotate
-	            # msg[4] # fire
-	            state["vy"] = scale(msg[6], macros.V_MAX)          # left/right trans
-	            state["pitch"] = scale(msg[7], macros.PITCH_BOUND)
-	            state["roll"] = 0 #scale(msg[7], macros.ROLL_BOUND)
-	            state["yaw"] = scale(msg[2], macros.YAW_BOUND)
-            	# bool(msg[9]) # aim mode
-            	# bool(msg[10]) # enable/disable
+    if (bool(state["useradio"])):
+    	if (ser.in_waiting > 0):
+            msg = ser.readline().decode('utf-8')
+            msg = [int(i) for i in msg.split(', ')]
+            #print(msg)
+            state["vx"] = scale(msg[3], macros.V_MAX)          # forward/backward trans
+            state["omega"] = scale(msg[4], macros.OMEGA_MAX)   # stationary rotate
+            # msg[4] # fire
+            state["vy"] = scale(msg[6], macros.V_MAX)          # left/right trans
+            state["pitch"] = scale(msg[7], macros.PITCH_BOUND)
+            state["roll"] = 0 #scale(msg[7], macros.ROLL_BOUND)
+            state["yaw"] = scale(msg[2], macros.YAW_BOUND)
+            state["enable"] = msg[8] > offset
+        	# bool(msg[9]) # aim mode
+        	# bool(msg[10]) # enable/disable
 
 def start_server():
     global app
@@ -70,8 +95,8 @@ def slidey():
     return ""
 
 sched = BackgroundScheduler()
-sched.add_job(fucking_loop, 'interval', seconds=dt)
-sched.add_job(fucking_teensy_loop, 'interval', seconds=dt)
+sched.add_job(fucking_loop, 'interval', seconds=state["timestep"])
+sched.add_job(fucking_teensy_loop, 'interval', seconds=state["timestep"])
 sched.start()
 
 start_server()
